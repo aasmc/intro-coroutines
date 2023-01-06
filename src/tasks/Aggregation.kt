dc3fa0d312ce034f52a5d79e4656309ae23e6b5a
aleksas <aasmc@yandex.ru>
2023-01-06 11:32:58 +0300

    Solve aggregation task

--- src/tasks/Aggregation.kt
+++ src/tasks/Aggregation.kt
@@ -3,7 +3,6 @@ package tasks
 import contributors.User
 
 /*
-TODO: Write aggregation code.
 
  In the initial list each user is present several times, once for each
  repository he or she contributed to.
@@ -14,5 +13,20 @@ TODO: Write aggregation code.
  The corresponding test can be found in test/tasks/AggregationKtTest.kt.
  You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
 */
-fun List<User>.aggregate(): List<User> =
-    this
+fun List<User>.aggregate(): List<User> = groupBy { user -> user.login }
+    .map { (login, group) ->
+        User(login, group.sumOf { it.contributions })
+    }.sortedByDescending { it.contributions }
+
+fun List<User>.aggregateAlt(): List<User> {
+    val map = hashMapOf<String, Int>()
+    for (u in this) {
+        map.merge(u.login, u.contributions, Int::plus)
+    }
+    val result = mutableListOf<User>()
+    for ((login, contributions) in map) {
+        result.add(User(login, contributions))
+    }
+    result.sortByDescending { it.contributions }
+    return result
+}
