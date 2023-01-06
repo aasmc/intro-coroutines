9f065153d4f83ab66ead1755f4a411e112594350
aleksas <aasmc@yandex.ru>
2023-01-06 12:59:07 +0300

    Solve task 7

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -1,9 +1,9 @@
 package tasks
 
 import contributors.*
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
+import java.util.concurrent.atomic.AtomicInteger
 
 suspend fun loadContributorsChannels(
     service: GitHubService,
@@ -11,6 +11,29 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: emptyList()
+
+        val channel = Channel<List<User>>()
+
+        repos.map { repo ->
+            async(Dispatchers.Default) {
+                val lst = service.getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+                channel.send(lst)
+            }
+        }
+
+        var result = emptyList<User>()
+
+        repeat(repos.size) {
+            val users = channel.receive()
+            result = (result + users).aggregate()
+            updateResults(result, it == repos.lastIndex)
+        }
     }
 }
