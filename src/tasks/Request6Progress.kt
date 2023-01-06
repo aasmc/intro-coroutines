38f2f7635496068810171ee09417821cf718ab9a
aleksas <aasmc@yandex.ru>
2023-01-06 12:44:56 +0300

    Solve tasks 5 and 6

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -7,5 +7,18 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: emptyList()
+
+    var result = emptyList<User>()
+    for ((idx, repo) in repos.withIndex()) {
+        val temp = service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+        result = (result + temp).aggregate()
+        updateResults(result, idx == repos.lastIndex)
+    }
 }
